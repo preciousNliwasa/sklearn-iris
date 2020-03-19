33d1570cf86ba394e7f8ac588b93a2fcb2cc1c54
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2020-03-19 01:50:32 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+model = pickle.load(open("model1.pkl","rb"))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    
+    
+    return render_template('index.html',prediction_text = "The plant is {}".format(prediction))
+    
+if __name__ == "__main__":
+    app.run(debug = True)
